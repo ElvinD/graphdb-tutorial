4e071814e3dfefdcd71bebdea7fa660ec0f38a82
Dechesne, Elvin <Elvin.Dechesne@ordina.nl>
2018-11-13 10:26:48 +0100

    Google Maps max map load exceeded (1) doh… cancelling this endeavour, but keeping the branch.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PersonComponent } from './person/person.component';
+import { AgmCoreModule, GoogleMapsAPIWrapper } from '@agm/core';
 import { MapComponent } from './map/map.component';
 
 @NgModule({
@@ -16,9 +17,12 @@ import { MapComponent } from './map/map.component';
   ],
   imports: [
     BrowserModule,
+    AgmCoreModule.forRoot({apiKey: 'AIzaSyBfzJHocmt3bsr0kUhDv0v-pfAXFSjeVy4'}),
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    GoogleMapsAPIWrapper
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, NgZone } from '@angular/core';
+import { MapsAPILoader, AgmMap } from '@agm/core';
+import { GoogleMapsAPIWrapper } from '@agm/core/services';
 import { SparqlService } from '../sparql.service';
 import { ListComponent } from '../list/list.component';
 
@@ -7,13 +9,61 @@ import { ListComponent } from '../list/list.component';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
+
 export class MapComponent extends ListComponent implements OnInit  {
 
-  constructor(protected sparqlService: SparqlService) {
+  geocoder: any;
+
+  constructor(protected sparqlService: SparqlService,
+              public mapsApiLoader: MapsAPILoader,
+              private zone: NgZone,
+              private wrapper: GoogleMapsAPIWrapper) {
     super(sparqlService);
- }
+    this.mapsApiLoader = mapsApiLoader;
+    this.zone = zone;
+    this.wrapper = wrapper;
+
+    this.mapsApiLoader.load().then(() => {
+      this.geocoder = new google.maps.Geocoder();
+    });
+  }
+  public location: Location = {
+    lat: 51.678418,
+    lng: 7.809007,
+    marker: {
+      lat: 51.678418,
+      lng: 7.809007,
+      draggable: true
+    },
+    zoom: 5
+  };
+
+  @ViewChild(AgmMap) map: AgmMap;
 
   ngOnInit() {
+    this.location.marker.draggable = true;
   }
 
+}
+
+declare var google: any;
+
+interface Marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
+}
+
+interface Location {
+  lat: number;
+  lng: number;
+  viewport?: Object;
+  zoom: number;
+  address_level_1?: string;
+  address_level_2?: string;
+  address_country?: string;
+  address_zip?: string;
+  address_state?: string;
+  marker?: Marker;
 }
